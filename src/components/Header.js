7f152dc72fe93882c9817ed80c3eb9d831eaa15e
AswinBarath <aswin2001barath@gmail.com>
2021-09-28 22:07:19 +0530

    Adding Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,74 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components";
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Header() {
     return (
-        <div>
-            header
-        </div>
+        <Container>
+            <a>
+                <img src="/images/logo.svg" alt="" />
+            </a>
+            <Menu>
+                <a href="#">Model S</a>
+                <a href="#">Model 3</a>
+                <a href="#">Model X</a>
+                <a href="#">Model Y</a>
+            </Menu>
+
+            <RightMenu>
+                <a href="#">Shop</a>
+                <a href="#">Tesla Account</a>
+                <CustomMenu />
+            </RightMenu>
+
+        </Container>
     )
 }
 
-export default Header
+export default Header;
+
+const Container = styled.div`
+    min-height: 60px;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+    top: 0;
+    left: 0;
+    right: 0;
+`;
+
+const Menu = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+
+    a {
+        font-weight: 600;
+        text-transform: uppercase;
+        padding: 0 10px;
+        flex-wrap: nowrap;
+    }
+
+    @media(max-width: 768px) {
+        display: none;
+    }
+
+`;
+
+const RightMenu = styled.div`
+    display: flex;
+    align-items: center;
+    a {
+        font-weight: 600;
+        text-transform: uppercase;
+        margin-right: 10px;
+        flex-wrap: nowrap;
+    }
+`;
+
+const CustomMenu = styled(MenuIcon)`
+    cursor: pointer;
+`;
